574030b6050ac96f13185d93f3bda91779c7c9ef
aydamst <aidamostafaei621@gmail.com>
2024-01-05 22:12:13 +0330

    selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -1,7 +1,15 @@
 def selectionSort(A):
     for i in range(len(A)):
-        min_idx = i
+        min_index = i
         for j in range(i + 1, len(A)):
-            if A[min_idx] > A[j]:
-                min_idx = j
-        A[i], A[min_idx] = A[min_idx], A[i]
+            if A[min_index] > A[j]:
+                min_index = j
+        A[i], A[min_index] = A[min_index], A[i]
+        return A
+    
+    
+    
+    
+araye=[1,2,6,9,8,3,4,8]
+sorting=selectionSort(araye)
+print(sorting)
