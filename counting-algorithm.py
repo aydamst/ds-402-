61e7f43d32801976d4c4d708a4fa68a24fedb1db
aydamst <aidamostafaei621@gmail.com>
2024-01-05 20:02:20 +0330

    creat new file

---
+++ counting-algorithm.py
@@ -0,0 +1,27 @@
+def counting_sort(arr):
+    
+    max_val = max(arr)
+    
+    
+    count = [0] * (max_val + 1)
+    
+   
+    for num in arr:
+        count[num] += 1
+  
+
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+    
+    return sorted_arr
+
+
+
+
+
+arr = [4, 9, 5, 0, 3, 9, 8, 7, 4, 0]
+sorted_arr = counting_sort(arr)
+print(sorted_arr)
+
+
